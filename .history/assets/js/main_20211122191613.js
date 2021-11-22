bd5d5a1efc34ca62dbe58c2a9c5ffdf78c563997
soniaarefin <soniaarefin7@gmail.com>
2021-11-22 21:17:35 +0600

    commit

---
+++ .history/assets/js/main_20211122191613.js
@@ -0,0 +1,28 @@
+// $(document).ready(function(){
+//     // responsive menu
+// 		$('.dropbtn').on('click', function(e) {
+// 			e.preventDefault();
+// 			$('.dropbtn').removeClass('js-nested-menu-opened');
+// 			$(this).toggleClass('js-nested-menu-opened');
+// 		});
+// 		// - responsive menu
+
+//         // ignore clicking on nested menu link
+// 	$('.menu-responsive-button').on('click', function(e) {
+// 		e.preventDefault();
+// 		$(this).toggleClass('js-menu-responsive-button-active');
+// 		$('.list-menu').toggleClass('js-menu-opened');
+// 		$('.dropbtn').removeClass('js-nested-menu-opened');
+// 	});
+// 	// - ignore clicking on nested menu link
+
+// });
+
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	}
+  } 
