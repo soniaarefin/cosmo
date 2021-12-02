bf3a003e091ad405d77fb085325ed1202102a608
soniaarefin <soniaarefin7@gmail.com>
2021-12-02 10:37:29 +0600

    last commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,14 +18,14 @@
 
 // });
 
-function myFunction() {
-	var x = document.getElementById("myTopnav");
-	if (x.className === "wrapper-menu") {
-	  x.className += " responsive";
-	} else {
-	  x.className = "wrapper-menu";
-	}
-  } 
+// function myFunction() {
+// 	var x = document.getElementById("myTopnav");
+// 	if (x.className === "wrapper-menu") {
+// 	  x.className += " responsive";
+// 	} else {
+// 	  x.className = "wrapper-menu";
+// 	}
+//   } 
 
 //   $('.projects-grid_container').masonry({
 //   // options
@@ -34,4 +34,46 @@ function myFunction() {
 // });
 
 
-}
+// var myTopnav = document.getElementById("myTopnav"),
+//     mobileToggle = document.getElementById("mobileToggle");
+
+// mobileToggle.onclick = function () {
+    
+//     "use strict";
+    
+//     myTopnav.classList.toggle("show_list");
+//     mobileToggle.classList.toggle("active");
+    
+// };
+var menuBar = document.getElementById("menuBar"),
+    mobileToggle = document.getElementById("mobileToggle");
+
+mobileToggle.onclick = function () {
+    
+    "use strict";
+    
+    menuBar.classList.toggle("show_list");
+    mobileToggle.classList.toggle("active");
+    
+};
+
+
+$(document).ready(function(){
+     // responsive menu
+ 		$('.dropbtn').on('click', function(e) {
+ 			e.preventDefault();
+ 			$('.dropbtn').removeClass('menu-bar');
+ 			$(this).toggleClass('mobileToggle');
+ 		});
+ 		// - responsive menu
+
+         // ignore clicking on nested menu link
+ 	$('.mobileToggle').on('click', function(e) {
+ 		e.preventDefault();
+		$(this).toggleClass('mobileToggle');
+ 		$('.list-menu').toggleClass('js-menu-opened');
+ 		$('.dropbtn').removeClass('js-nested-menu-opened');
+ 	});
+ 	// - ignore clicking on nested menu link
+
+ });
